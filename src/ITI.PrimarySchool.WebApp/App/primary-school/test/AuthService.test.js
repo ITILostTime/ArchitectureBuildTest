84eac976b2056b3367ab936d9c5489a485171a2b
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-18 15:24:39 +0200

    set up js test; setup .netTest

---
+++ src/ITI.PrimarySchool.WebApp/App/primary-school/test/AuthService.test.js
@@ -0,0 +1,28 @@
+/*
+const AuthService = require('./../src/services/AuthService');
+import AuthService from './../src/services/AuthService'
+jest.mock('shortid');
+import shortid from 'shortid';
+
+describe('mocks shortid', () => {
+  it('works', () => {
+    shortid.mockImplementation(() => 1);
+   
+  });
+});
+test('test logOff route', () => {
+  AuthService.logoutEndpoint = '/Account/LogOff';
+  expect(AuthService.logoutEndpoint).toEqual('/Account/LogOff');
+});
+*/
+
+
+/*
+const AuthService = require('./../src/services/AuthService');
+
+const app = require('./../src/app');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(app.sum(1, 2)).toBe(3);
+});
+*/
