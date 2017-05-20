0634309910199485059d1029157dfb75169b9853
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-20 14:21:11 +0200

    test again

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -1,5 +1,5 @@
 BRANCH="release"
-SEMVER="0.0.1"
+SEMVER="0.0.1" #VERY BAD !
 
 # Are we on the right branch?
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
@@ -11,8 +11,8 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Starting to tag commit.\n"
       #set the losttime adress ?
-      git config --global user.email "travis@travis-ci.org"
-      git config --global user.name "Travis"
+      #git config --global user.email "travis@travis-ci.org"
+      #git config --global user.name "Travis"
 
       #merge the release into master with the tag
 
@@ -20,7 +20,6 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       git tag -a v$SEMVER-${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       git push origin --tags
       git fetch origin
-
       echo -e "Done magic with tags.\n"
   fi
   fi
