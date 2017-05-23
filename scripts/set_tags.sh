9bf3d49029506cf4790f19d2bb068bd36a59cb60
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-23 11:56:13 +0200

    changed token system

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -3,10 +3,6 @@ SEMVER="0.0.1" #VERY BAD !
 
 # Are we on the right branch?
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
-  
-  # Is this not a Pull Request?
-  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
-    
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Starting to tag commit.\n"
@@ -19,9 +15,8 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
 
       # Add tag and push to release.
       git tag -a v$SEMVER-${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
-      git push origin --tags
+      git push origin --tags --repo="${GIT_DEPLOY_REPO}"
       git fetch origin
       echo -e "Done magic with tags.\n"
-  fi
   fi
 fi
