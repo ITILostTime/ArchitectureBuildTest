a4111878c5d4e7a90b6e392798db89bd0be921f9
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-23 16:46:42 +0200

    changed tag

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -1,5 +1,5 @@
 BRANCH="release"
-SEMVER="0.0.10" #VERY BAD !
+SEMVER="0.0.11" #VERY BAD !
 VERSIONTAG="v$SEMVER-${TRAVIS_BUILD_NUMBER}"
 
 # Are we on the right branch?
@@ -7,14 +7,15 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Travis tag is : $TRAVIS_TAG"
-      echo -e "tag commit : $VERSIONTAG \n"
+      echo -e "tag of the commit : $VERSIONTAG \n"
       # Add tag and push to release.
       git tag -a $VERSIONTAG -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       echo -e "push to : ${GIT_DEPLOY_REPO}"
-      git push $VERSIONTAG --repo="${GIT_DEPLOY_REPO}"
+      git push origin $VERSIONTAG --repo="${GIT_DEPLOY_REPO}"
       git fetch --tags
-      TRAVIS_TAG=$VERSIONTAG
       echo -e "Travis tag is : $TRAVIS_TAG"
+      TRAVIS_TAG=$VERSIONTAG
+      echo -e "Travis tag is now : $TRAVIS_TAG"
      if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Tag empty \n"
       else
