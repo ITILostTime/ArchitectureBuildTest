a8c3e01fcdf0ecd38229e0099028622cedd99ab2
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-23 16:54:56 +0200

    change again

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -1,5 +1,5 @@
 BRANCH="release"
-SEMVER="0.0.11" #VERY BAD !
+SEMVER="0.0.10" #VERY BAD !
 VERSIONTAG="v$SEMVER-${TRAVIS_BUILD_NUMBER}"
 
 # Are we on the right branch?
@@ -11,15 +11,15 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       # Add tag and push to release.
       git tag -a $VERSIONTAG -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       echo -e "push to : ${GIT_DEPLOY_REPO}"
-      git push origin $VERSIONTAG --repo="${GIT_DEPLOY_REPO}"
+      git push $VERSIONTAG --repo="${GIT_DEPLOY_REPO}"
       git fetch --tags
       echo -e "Travis tag is : $TRAVIS_TAG"
       TRAVIS_TAG=$VERSIONTAG
       echo -e "Travis tag is now : $TRAVIS_TAG"
-     if [ -z "$TRAVIS_TAG" ]; then
-      echo -e "Tag empty \n"
-      else
-      echo -e "Tag $TRAVIS_TAG added, pushed and fetched. \n"
-     fi
+      if [ -z "$TRAVIS_TAG" ]; then
+        echo -e "Tag empty \n"
+        else
+        echo -e "Tag $TRAVIS_TAG added, pushed and fetched. \n"
+      fi
   fi
 fi
