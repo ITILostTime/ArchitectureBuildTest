b0346a791bdf706e64386ccba375fd6946efe1c0
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-23 15:33:31 +0200

    re-added tokens; changed ver ; changed tagsystem

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -1,10 +1,11 @@
 BRANCH="release"
-SEMVER="0.0.1" #VERY BAD !
+SEMVER="0.0.8" #VERY BAD !
 
 # Are we on the right branch?
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
+      echo -e "$TRAVIS_TAG"
       echo -e "Starting to tag commit.\n"
       # Add tag and push to release.
       git tag -a v$SEMVER-${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
@@ -12,9 +13,11 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       echo -e "github key : ${GITHUBKEY}"
       echo -e "push with : ${GIT_DEPLOY_REPO}"
       git push --tags --repo="${GIT_DEPLOY_REPO}"
-      git fetch origin
+      git fetch --tags
      if [ ! -z "$TRAVIS_TAG" ]; then
       echo -e "Tag added, pushed and fetched.\n"
+      else
+      echo -e "NOPE \n"
      fi
   fi
 fi
