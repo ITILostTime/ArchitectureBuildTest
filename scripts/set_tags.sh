15d76e6f77cedc934766b34a103272449110c05e
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-24 10:13:09 +0200

    cleaned listener

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -9,12 +9,9 @@ VERSIONTAG="v$SEMVER-${TRAVIS_BUILD_NUMBER}"
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
-      echo -e "Travis tag is : $TRAVIS_TAG"
       echo -e "tag of the commit : $VERSIONTAG \n"
       # Add tag and push to release.
       git tag -a $VERSIONTAG -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
-      echo -e "Travis tag is now : $TRAVIS_TAG"
-
       echo -e "push to : ${GIT_DEPLOY_REPO}"
       git push --tags --repo="${GIT_DEPLOY_REPO}"
       git fetch --tags
